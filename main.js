ef95a5bd251a762279101ef724f869088c6250ab
Vika230 <viktory.pashynska@gmail.com>
2026-05-20 20:28:51 +0300

    pr2

--- main.js
+++ main.js
@@ -86,5 +86,54 @@ started=true;
 
 });
 
+const scrollTopBtn=document.querySelector('.scroll-top');
 
+window.addEventListener('scroll',()=>{
 
+if(window.scrollY>500){
+scrollTopBtn.classList.add('active');
+}
+
+else{
+scrollTopBtn.classList.remove('active');
+}
+
+});
+
+scrollTopBtn.addEventListener('click',()=>{
+
+window.scrollTo({
+top:0,
+behavior:'smooth'
+});
+
+});
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav a");
+
+window.addEventListener("scroll", () => {
+
+let current = "";
+
+sections.forEach(section => {
+
+const sectionTop = section.offsetTop;
+
+if(scrollY >= sectionTop - 150){
+current = section.getAttribute("id");
+}
+
+});
+
+navLinks.forEach(link => {
+
+link.classList.remove("active");
+
+if(link.getAttribute("href") === "#" + current){
+link.classList.add("active");
+}
+
+});
+
+});
